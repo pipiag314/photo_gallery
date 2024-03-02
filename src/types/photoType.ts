40b78918004e4b5fa6df55130355f6e72ddaf3ba
gela pipia <gelapipia10@gmail.com>
2024-03-03 00:57:44 +0400

    ADD: types

---
+++ src/types/photoType.ts
@@ -0,0 +1,20 @@
+export type PhotoType = {
+    alt_description: string;
+    created_at: string;
+    height: number;
+    width: number;
+    description: string;
+    id: string;
+    likes: number;
+    slug: string;
+    urls: {
+        full: string;
+        raw: string;
+        regular: string;
+        small: string;
+        small_s3: string;
+        thumb: string;
+    }
+}
+
+
